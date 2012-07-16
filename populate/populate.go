e8b7361b551afca284a0714b7d9d5bf444cffce5
Shawn Smith <shawnpsmith@gmail.com>
2012-07-15 18:31:42 -0700

    change path of kanjidic2.xml file in populate.go

--- populate/populate.go
+++ populate/populate.go
@@ -13,7 +13,7 @@ func init() {
 
 func populate(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	kanjidic := kanjidic2.ParseKanjiDic2("/Users/shawn/kanjihub/kanjidic2/kanjidic2.xml")
+	kanjidic := kanjidic2.ParseKanjiDic2("kanjidic2/kanjidic2.xml")
 	for _, kanji := range kanjidic {
 		_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Kanji", nil), &kanji)
 		if err != nil {
