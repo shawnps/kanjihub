f8ed3b565242cf60fa557588d820a11fe2c4dcf8
Shawn Smith <shawnpsmith@gmail.com>
2012-07-15 23:07:18 -0700

    add kanjidic2.xml file

--- populate/populate.go
+++ populate/populate.go
@@ -24,7 +24,7 @@ type Kanji struct {
 
 func populate(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	kanjidic := kanjidic2.ParseKanjiDic2("/Users/shawn/kanjihub/kanjidic2/kanjidic2.xml")
+	kanjidic := kanjidic2.ParseKanjiDic2("kanjihub/kanjidic2/kanjidic2.xml")
 	for _, kanji := range kanjidic {
 		k := Kanji{
 			Literal:     kanji.Literal,
