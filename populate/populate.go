72c2f2559f8b942cf36eaf1d88f2c31dd45e8156
Shawn Smith <shawnpsmith@gmail.com>
2012-07-15 23:06:44 -0700

    update populate handler to store onyomi, kunyomi, meanings

--- populate/populate.go
+++ populate/populate.go
@@ -11,11 +11,43 @@ func init() {
 	http.HandleFunc("/populate", populate)
 }
 
+type Kanji struct {
+	Literal     string
+	Grade       int
+	StrokeCount int
+	Freq        int
+	JLPT        int
+	Onyomi      []string
+	Kunyomi     []string
+	Meanings    []string
+}
+
 func populate(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	kanjidic := kanjidic2.ParseKanjiDic2("kanjidic2/kanjidic2.xml")
+	kanjidic := kanjidic2.ParseKanjiDic2("/Users/shawn/kanjihub/kanjidic2/kanjidic2.xml")
 	for _, kanji := range kanjidic {
-		_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Kanji", nil), &kanji)
+		k := Kanji{
+			Literal:     kanji.Literal,
+			Grade:       kanji.Grade,
+			StrokeCount: kanji.StrokeCount,
+			Freq:        kanji.Freq,
+			JLPT:        kanji.JLPT,
+		}
+		for _, r := range kanji.Readings {
+			if r.RType == "ja_on" {
+				k.Onyomi = append(k.Onyomi, r.Value)
+			}
+			if r.RType == "ja_kun" {
+				k.Kunyomi = append(k.Kunyomi, r.Value)
+			}
+		}
+		for _, m := range kanji.Meanings {
+			// English meanings do not have an m_lang attribute
+			if m.MLang == "" {
+				k.Meanings = append(k.Meanings, m.Value)
+			}
+		}
+		_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Kanji", nil), &k)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
