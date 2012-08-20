d92557f09b3ab466b7c411edba4dadfb3bcc5ea9
Ed Rooth <ed.rooth@gmail.com>
2012-08-19 18:16:54 -0700

    now grouping by stroke count in search result list

--- frontend/public/js/lib/handlebars.js
+++ frontend/public/js/lib/handlebars.js
@@ -16,5 +16,30 @@ function (Handlebars) {
     }
   });
 
+  /**
+   * {{#each_with_prev_value array property}}
+   *   <li>{{myProp}}}</li>
+   * {{/each_with_prev_value}}
+   */
+  Handlebars.registerHelper('each_with_prev_value',
+  function(array, propName, options) {
+    var buffer = '', i, len, item;
+    for (i = 0, len = array.length; i < len; i += 1) {
+      item = array[i];
+      item.prevDifferent = true;
+      if (i > 0) {
+        item.prev = array[i-1][propName];
+        if (item.prev === item[propName]) {
+          item.prevDifferent = false;
+        }
+      }
+      // show the inside of the block
+      buffer += options.fn(item);
+    }
+
+    // return the finished buffer
+    return buffer;
+  });
+
   return Handlebars;
 });
